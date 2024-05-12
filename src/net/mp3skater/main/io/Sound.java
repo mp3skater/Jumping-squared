bf90a460976fdb0b1fcc45c01181d1f7bc855956
Benji <148818861+23paudav@users.noreply.github.com>
2024-05-12 19:57:47 +0200

    Updated Sound Volume

--- src/net/mp3skater/main/io/Sound.java
+++ src/net/mp3skater/main/io/Sound.java
@@ -5,10 +5,14 @@ import java.net.URL;
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
+import javax.sound.sampled.FloatControl;
 
-    public class Sound {
+public class Sound {
         Clip clip;
         URL[] soundURL = new URL[30];
+        FloatControl fc;
+        public int volumeScale = 3;
+        float volume;
 
         public Sound() {
             soundURL[0] = getClass().getResource("/sound/bgmusic1_leise.wav");
@@ -28,6 +32,8 @@ import javax.sound.sampled.Clip;
                 AudioInputStream ais = AudioSystem.getAudioInputStream(soundURL[i]);
                 clip = AudioSystem.getClip();
                 clip.open(ais);
+                fc = (FloatControl)clip.getControl(FloatControl.Type.MASTER_GAIN);
+                checkVolume();
             } catch (Exception e) {
                 e.printStackTrace();
             }
@@ -44,6 +50,18 @@ import javax.sound.sampled.Clip;
         public void stop() {
             if(clip != null)
                 clip.stop();
+        }
+        public void checkVolume(){
+
+            switch (volumeScale){
+                case 0: volume = -80f; break;
+                case 1: volume = -23f; break;
+                case 2: volume = -12f; break;
+                case 3: volume = -5f; break;
+                case 4: volume = 1f; break;
+                case 5: volume = 6f; break;
+            }
+            fc.setValue(volume);
         }
     }
 
