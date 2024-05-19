22c3bd218baf82f290ba62a4280b13b384db1ee1
mp3skater <mp3skaterr@gmail.com>
2024-05-19 10:52:13 +0200

    perfect movement again

--- src/net/mp3skater/main/obj/Obj_player.java
+++ src/net/mp3skater/main/obj/Obj_player.java
@@ -1,6 +1,7 @@
 package net.mp3skater.main.obj;
 
 import net.mp3skater.main.GamePanel;
+import net.mp3skater.main.utils.Col_Utils;
 import net.mp3skater.main.utils.Draw_Utils;
 import net.mp3skater.main.io.KeyHandler;
 import net.mp3skater.main.utils.Sound_Utils;
@@ -58,13 +59,13 @@ public class Obj_player extends Obj_moving {
 
         // Dev tools:
         // Reset camera
-        if(KeyHandler.zeroPressed) GamePanel.offset = pos[0] - GamePanel.WIDTH/2.0;
+        //if(KeyHandler.zeroPressed) GamePanel.offset = pos[0] - GamePanel.WIDTH/2.0;
         // Change level
-        if(KeyHandler.onePressed) GamePanel.loadLevel(1);
-        else if(KeyHandler.twoPressed) GamePanel.loadLevel(2);
-        else if(KeyHandler.threePressed) GamePanel.loadLevel(3);
-        else if(KeyHandler.fourPressed) GamePanel.loadLevel(4);
-        else if(KeyHandler.fivePressed) GamePanel.loadLevel(5);
+        //if(KeyHandler.onePressed) GamePanel.loadLevel(1);
+        //else if(KeyHandler.twoPressed) GamePanel.loadLevel(2);
+        //else if(KeyHandler.threePressed) GamePanel.loadLevel(3);
+        //else if(KeyHandler.fourPressed) GamePanel.loadLevel(4);
+        //else if(KeyHandler.fivePressed) GamePanel.loadLevel(5);
 
         // Collision with left side of the screen
         if(pos[0]+vec[0]-(int)GamePanel.offset < 0)
@@ -86,62 +87,98 @@ public class Obj_player extends Obj_moving {
             GamePanel.level++;
             GamePanel.loadLevel(GamePanel.level);
         }
-
+        // Variables to check nearest collision
+        Obj nearX = null, nearY = null;
         // For all elements you could collide with in walls
         for(Obj w : GamePanel.walls) {
             // Return if endbar to allow the player to finish the level
             if(w instanceof Obj_endBar)
                 continue;
-
             // Test weather going vertically, horizontally or both would make you collide with something
             // Horizontally
             if(vec[0] != 0)
-                if(collides(w.getX()-vec[0], w.getY(), (int)w.getSX(), (int)w.getSY())) {
-                    xCollision((int)w.getX(), vec[0]<0/*going left*/? w.size[0] : 0);
+                if(collides(w.getX()-Col_Utils.absRound(vec[0]), w.getY(), (int)w.getSX(), (int)w.getSY())) {
+                    nearX = nearX==null? w : getNearestX(nearX, w, vec[0] > 0);
                 }
             // Vertically
             if(vec[1] != 0)
-                if(collides(w.getX(), w.getY()-vec[1], (int)w.getSX(), (int)w.getSY())) {
-                    if(reverseGravity? vec[1]<0 : vec[1]>0) onGround = true;
-                    yCollision((int)w.getY(), vec[1]<0/*going up*/? w.size[1] : 0);
+                if(collides(w.getX(), w.getY()-Col_Utils.absRound(vec[1]), (int)w.getSX(), (int)w.getSY())) {
+                    if(vec[1]>0)
+                        onGround = true;
+                    nearY = nearY==null? w : getNearestY(nearY, w, vec[1] > 0);
                 }
             // Both
-            if(vec[0] != 0 && vec[1] != 0)
-                if(collides(w.getX()-vec[0], w.getY()-vec[1], (int)w.getSX(), (int)w.getSY())) {
-                    // To avoid player from getting stuck
-                    if(!(!collides(w.getX()-vec[0], w.getY(), (int)w.getSX(), (int)w.getSY()) &&
-                            !collides(w.getX(), w.getY()-vec[1], (int)w.getSX(), (int)w.getSY())))
-                        xCollision((int)w.getX(), vec[0]<0/*going left*/? w.size[0] : 0);
-                    yCollision((int)w.getY(), vec[1]<0/*going up*/? w.size[1] : 0);
-                }
+            //if(vec[0] != 0 && vec[1] != 0)
+            //    if(collides((int)(w.getX()-vec[0]), (int)(w.getY()-vec[1]), (int)w.getSX(), (int)w.getSY())) {
+            //        System.out.println("colXY");
+            //        // To avoid player from getting stuck
+            //        if(!(!collides((int)(w.getX()-vec[0]), (int)w.getY(), (int)w.getSX(), (int)w.getSY()) &&
+            //                !collides((int)w.getX(), w.getY()-vec[1], (int)w.getSX(), (int)w.getSY())))
+            //            nearX = nearX==null? w : getNearestX(nearX, w, vec[0] > 0);
+            //        nearY = nearY==null? w : getNearestY(nearY, w, vec[1] > 0);
+            //    }
         }
         // For all elements you could collide with in platforms
         for(Obj p : GamePanel.platforms) {
-            if(p == null) continue;
-
+            if(p == null)
+                continue;
             // Test weather going vertically, horizontally or both would make you collide with something
             // Horizontally
             if(vec[0] != 0)
-                if(collides(p.getX()-vec[0], p.getY(), (int)p.getSX(), (int)p.getSY())) {
-                    xCollision((int)p.getX(), vec[0]<0/*going left*/? p.size[0] : 0);
+                if(collides(p.getX()- Col_Utils.absRound(vec[0]), p.getY(), (int)p.getSX(), (int)p.getSY())) {
+                    nearX = nearX==null? p : getNearestX(nearX, p, vec[0] > 0);
                 }
             // Vertically
             if(vec[1] != 0)
-                if(collides(p.getX(), p.getY()-vec[1], (int)p.getSX(), (int)p.getSY())) {
-                    if(reverseGravity? vec[1]<0 : vec[1]>0) onGround = true;
-                    yCollision((int)p.getY(), vec[1]<0/*going up*/? p.size[1] : 0);
+                if(collides(p.getX(), p.getY()-Col_Utils.absRound(vec[1]), (int)p.getSX(), (int)p.getSY())) {
+                    if(vec[1]>0)
+                        onGround = true;
+                    nearY = nearY==null? p : getNearestY(nearY, p, vec[1] > 0);
                 }
             // Both
-            if(vec[0] != 0 && vec[1] != 0)
-                if(collides(p.getX()-vec[0], p.getY()-vec[1], (int)p.getSX(), (int)p.getSY())) {
-                    // To avoid player from getting stuck
-                    if(!(!collides(p.getX()-vec[0], p.getY(), (int)p.getSX(), (int)p.getSY()) &&
-                            !collides(p.getX(), p.getY()-vec[1], (int)p.getSX(), (int)p.getSY())))
-                        xCollision((int)p.getX(), vec[0]<0/*going left*/? p.size[0] : 0);
-                    yCollision((int)p.getY(), vec[1]<0/*going up*/? p.size[1] : 0);
-                }
+            //if(vec[0] != 0 && vec[1] != 0)
+            //    if(collides((int)(p.getX()-vec[0]), (int)(p.getY()-vec[1]), (int)p.getSX(), (int)p.getSY())) {
+            //        // To avoid player from getting stuck
+            //        if(!(!collides((int)(p.getX()-vec[0]), (int)p.getY(), (int)p.getSX(), (int)p.getSY()) &&
+            //                !collides((int)p.getX(), p.getY()-vec[1], (int)p.getSX(), (int)p.getSY())))
+            //            nearX = nearX==null? p : getNearestX(nearX, p, vec[0] > 0);
+            //        nearY = nearY==null? p : getNearestY(nearY, p, vec[1] > 0);
+            //    }
+        }
+        //if(nearX != null)
+        //    System.out.printf("x: "+(int)nearX.getX()+" ");
+        //if(nearY != null)
+        //    System.out.printf("y: "+nearY.getY());
+        // Actual collision
+        // Actual collision:
+
+        // To avoid players from getting stuck
+        if (nearX != null)
+            if (nearY != null && nearX == nearY && (int) nearX.getX() == nearX.getX() && (int) nearY.getY() == nearY.getY()) {
+                yCollision((int) nearY.getY(), vec[1] < 0/*going up*/ ? nearY.size[1] : 0);
+                return;
+            }
+
+        if(nearX!=null) {
+            xCollision((int)nearX.getX(), vec[0]<0/*going left*/? nearX.size[0] : 0);
+        }
+        if(nearY!=null) {
+            yCollision((int)nearY.getY(), vec[1]<0/*going up*/? nearY.size[1] : 0);
         }
     }
+    /*
+    Returns the nearest Obj to collide with
+    <positive> means the player is going right or down
+     */
+    private Obj getNearestX(Obj o1, Obj o2, boolean positive) {
+        if(positive) return o1.getX()<o2.getY()? o1 : o2;
+        else return o1.getX()+o1.getSX()>o2.getX()+o2.getSX()? o1 : o2;
+    }
+    private Obj getNearestY(Obj o1, Obj o2, boolean positive) {
+        if(positive) return o1.getY()<o2.getY()? o1 : o2;
+        else return o1.getY()+o1.getSY()>o2.getY()+o2.getSY()? o1 : o2;
+    }
+
 
     /*
     Overrides the update class to include the <movement()> and the <turndownvec()> methods
